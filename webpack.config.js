bc50411e46e87e02a541cb6ccffd5367212b5454
Jon Rogers <jonrogers121@gmail.com>
2022-06-04 18:28:22 +0100

    Added config page

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ require('dotenv').config({ path: './.env' });
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
-  output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js' },
+  output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js', publicPath: '/' },
   mode: process.env.NODE_ENV || 'development',
   resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
   devServer: { static: path.join(__dirname, 'src'), historyApiFallback: true },
