6265de814ec32f2a4c841f49d933b4eeb5eeaa57
Jon Rogers <jonrogers121@gmail.com>
2022-06-04 10:19:24 +0100

    Initial yarn install

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import "./global.css"
+import "./global.css";
 
 /**
  * The starting page for your App
  */
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <>
         <BrowserRouter>
           <Header />
           <main>
             <section>
-                <Routes>
-                  <Route path={"/"} element={<Home />} />
-                </Routes>
+              <Routes>
+                <Route path={"/"} element={<Home />} />
+              </Routes>
             </section>
           </main>
         </BrowserRouter>
