d2dafee63e09c8101302891313b2912917bde01d
Jon Rogers <jonrogers121@gmail.com>
2022-06-05 21:04:01 +0100

    Added search page

--- e2etests/search_page.spec.ts
+++ e2etests/search_page.spec.ts
@@ -6,4 +6,9 @@ test('it allows a film to be entered and will show results by query', async ({ p
   await page.locator('input').fill('Jaws');
   await page.locator('text=Find Movies').click();
   await expect(page).toHaveURL('http://localhost:8080/search/Jaws');
+  // check result cards link to details page
+  await page.locator('[data-testid="movieCard-0-header"]').click();
+  await expect(page).toHaveURL('https://tmdb-test-app.netlify.app/movie/578');
+  const movieDetailsHeader = page.locator('data-test-id=movieCard-0');
+  await expect(movieDetailsHeader).toBeDefined();
 });
