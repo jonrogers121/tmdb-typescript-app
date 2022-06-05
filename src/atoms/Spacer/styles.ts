458bafedbb962b444ef23906b465c97dcfd5391c
Jon Rogers <jonrogers121@gmail.com>
2022-06-05 10:12:04 +0100

    Added variable breakpoint

--- src/atoms/Spacer/styles.ts
+++ src/atoms/Spacer/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { px2Rem } from '../../helpers/px2Rem';
+import { breakpoints } from '../../variables/breakpoints';
 import { ISpacer } from './types';
 
 const StyledSpacer = styled.div<ISpacer>`
   margin-bottom: ${({ height, mobile }) => height && !mobile && px2Rem(height)};
-  @media (max-width: 460px) {
+  @media (max-width: ${breakpoints.mobile}px) {
     margin-bottom: ${({ height, mobile }) => height && mobile && px2Rem(height)};
   }
 `;
